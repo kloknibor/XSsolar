4db28a95f681644fded081c4efc8237d1a05245e
Robin <robinkolk@msn.com>
2019-04-03 10:41:23 +0200

    update to 0.7

---
+++ build and upload.bat
@@ -0,0 +1,2 @@
+C:\Users\robin.kolk\AppData\Local\Programs\Python\Python37\python.exe setup.py bdist_wheel
+C:\Users\robin.kolk\AppData\Local\Programs\Python\Python37\python.exe -m twine upload dist/*
